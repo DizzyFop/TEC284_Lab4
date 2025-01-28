27aa6134b5cd63e469f5d4e6e7ce3633f9e3e074
Jack <54298425+DizzyFop@users.noreply.github.com>
2025-01-28 14:37:37 -0600

    Basic Temp and Humidity display

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -5,7 +5,10 @@
 
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
+DHT dht(3, DHT11);
+
 void setup() {
+dht.begin();
 u8x8.begin();
 u8x8.setFlipMode(1);
 u8x8.clear();
@@ -13,5 +16,10 @@ u8x8.clear();
 void loop() {
 u8x8.setFont(u8x8_font_chroma48medium8_r);
 u8x8.setCursor(0, 0);
-u8x8.print("Hello World!");
+
+float temp = dht.readTemperature();
+float humidity = dht.readHumidity();
+
+u8x8.println(temp);
+u8x8.print(humidity);
 }
