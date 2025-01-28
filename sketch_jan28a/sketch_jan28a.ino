70b2995040bba16cf8eb0a5f92f5e1fc01714f2b
Jack <54298425+DizzyFop@users.noreply.github.com>
2025-01-28 15:10:48 -0600

    Added alarm and turn on light when humidity is above 60

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -12,6 +12,7 @@ dht.begin();
 u8x8.begin();
 u8x8.setFlipMode(1);
 u8x8.clear();
+pinMode (4, OUTPUT);
 }
 void loop() {
 u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -25,4 +26,15 @@ u8x8.println((temp * 9/5) + 32);
 u8x8.println("Humidity: ");
 u8x8.print(humidity);
 u8x8.print("%");
+
+if(humidity > 60){
+digitalWrite(4, HIGH);
+tone(5,1000,100);
+}
+else{
+digitalWrite(4, LOW);
+}
+
+
+//noTone(5);
 }
